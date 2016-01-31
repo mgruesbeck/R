6e23778baa80b1be34cf5504f61ce8ea22f8e0d0
mgruesbeck <mgruesbeck@gmail.com>
2016-01-31 15:14:24 -0800

    add code comments

--- 1-scatterplot/scatterplot.r
+++ 1-scatterplot/scatterplot.r
@@ -1,10 +1,17 @@
 #!/usr/bin/env Rscript
 
+
+# load awesome things
 library(ggplot2)
 library(ggthemes)
 
+# set data frame to read csv
 data <- read.csv('scatterplot.csv')
+
+# create png
 png('scatterplot.png', width=400, height=400)
+
+# use ggplot
 ggplot(data, aes(x=x,y=y)) + geom_point() + 
     theme_economist() + 
     scale_colour_economist() 
--- 2-datestamps/time_series.r
+++ 2-datestamps/time_series.r
@@ -1,12 +1,20 @@
 #!/usr/bin/env Rscript
 
+# load in the thangs
 library(ggplot2)
 library(scales)
 library(ggthemes)
 
+# read csv files
 data <- read.csv('time_series.csv')
+
+# set timestamp
 data$timestamp_obj <- as.POSIXct(data$timestamp, format="%Y-%m-%d %H:%M:%S")
+
+# create svg
 svg('time_series.svg', width=8, height=5)
+
+# use ggplot
 ggplot(data, aes(x=timestamp_obj,y=delay)) + geom_area() + 
     theme_economist() + 
     scale_colour_economist() +
